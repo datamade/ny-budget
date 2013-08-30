c4b71b2ae370743a52505c8641ba3f67ae497fc9
Eric van Zanten <eric@bahai.us>
2013-08-30 16:59:00 -0500

    Got basic collection function going. Added a couple helper libraries.

--- js/app.js
+++ js/app.js
@@ -9,20 +9,36 @@ var AppRouter = Backbone.Router.extend({
     }
 });
 
-// Instantiate the router
+var BudgetColl = new Backbone.Collection;
+
+BudgetColl.getTotalExp = function(year){
+    var total = []
+    this.forEach(function(item){
+        var amount = item.get('Expenditures ' + year);
+        total.push(accounting.unformat(amount));
+    })
+    return total.reduce(function(a,b){return a + b});
+}
+
 var app_router = new AppRouter;
-app_router.on('route:routeSearch', function (query) {
-    BudgetLib.renderSearch(query); 
-});
-app_router.on('route:routeView', function (mode, name, year, viewChart) {
-    BudgetLib.updateView(mode, name, year, viewChart, true); 
-});
 app_router.on('route:defaultRoute', function (actions) {
-    BudgetLib.updateView('home', 'default', 2013, 'list', true); 
+    d3.csv('data/macoupin_budget_cleaned.csv', function(rows){
+        BudgetColl.reset(rows);
+        BudgetLib.updateView('home', 'default', 2013, 'list', true);
+    })
 });
 
-// Start Backbone history a necessary step for bookmarkable URL's
-Backbone.history.start();  
+
+// Instantiate the router
+//  app_router.on('route:routeSearch', function (query) {
+//      BudgetLib.renderSearch(query);
+//  });
+//  app_router.on('route:routeView', function (mode, name, year, viewChart) {
+//      BudgetLib.updateView(mode, name, year, viewChart, true);
+//  });
+//
+//  // Start Backbone history a necessary step for bookmarkable URL's
+Backbone.history.start();
 
 // Additional setup
 $("#search-query").ezpz_hint();
@@ -49,5 +65,5 @@ $("#search-query").keydown(function(e){
 $('#search').click(function(){
   var query = $('#search-query').val();
   app_router.navigate("search/" + query, {trigger: false});
-  BudgetLib.renderSearch(query); 
+  BudgetLib.renderSearch(query);
 });
