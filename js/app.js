1b466b394dbb4efbf05a55a777731f0dc58ab82c
Eric van Zanten <eric@bahai.us>
2013-11-08 11:18:33 -0600

    Stubbed out search function. It's searching and working pretty well. Need to see about how to actually display the results.

--- js/app.js
+++ js/app.js
@@ -196,6 +196,7 @@
                         }
                         self.updateTables(topLevelView, name, filter);
                     }
+                    self.searchView = new app.SearchView();
                 }
             );
         },
@@ -599,6 +600,31 @@
             var clickedYear = new Date(x).getFullYear();
             var yearIndex = this.series.processedYData.indexOf(y);
             collection.updateYear(clickedYear, yearIndex);
+        }
+    });
+
+    app.SearchView = Backbone.View.extend({
+        el: $('#search-form'),
+        initialize: function(){
+            var search_options = {
+                keys: ['Expense Line'],
+                threshold: 0.4
+            }
+            this.Search = new Fuse(collection.toJSON(), search_options);
+            this.render();
+        },
+        events: {
+            'click #search': 'engage'
+        },
+        render: function(){
+            this.$el.html(template_cache('search'));
+        },
+        engage: function(e){
+            e.preventDefault();
+            var input = $(e.currentTarget).parent().prev();
+            var term = $(input).val();
+            var results = this.Search.search(term);
+            console.log(results);
         }
     });
 
