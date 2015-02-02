e1f4d213a3b809d2de369e831d25d4f6af9e96d0
Cathy Deng <cathyd125@gmail.com>
2015-02-02 14:51:31 -0600

    add info on budget figures

--- js/app.js
+++ js/app.js
@@ -1069,4 +1069,7 @@
     var collection = new app.BudgetColl();
     var app_router = new app.Router({collection: collection});
     Backbone.history.start();
+    $(document).ready(function() {
+        $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+    });
 })()
