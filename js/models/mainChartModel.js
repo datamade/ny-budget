203735aac0f151ac3a9639d50022f0aab01303d6
Cathy Deng <cathyd125@gmail.com>
2015-02-19 13:56:51 -0600

    update summary section appropriately w/ inflation adjustment

--- js/models/mainChartModel.js
+++ js/models/mainChartModel.js
@@ -1,13 +1,25 @@
 app.MainChartModel = Backbone.Model.extend({
     setYear: function(year, index){
         console.log("*** in MainChartModel setYear")
-        var actual = this.get('actuals');
-        var est = this.get('estimates');
-        var actualChange = BudgetHelpers.calc_change(actual[index], actual[index - 1]);
-        var estChange = BudgetHelpers.calc_est_change(est[index], est[index - 1], actual[index - 1]);
+        var actuals = this.get('actuals');
+        var ests = this.get('estimates');
+
+        // these are the values that will be displayed in summary section
+        var sel_actual_sum = actuals[index]
+        var sel_est_sum = ests[index]
+        var prev_actual_sum = actuals[index-1]
+        var prev_est_sum = ests[index-1]
+        if(this.get('isInflationAdjusted')){
+            sel_actual_sum = BudgetHelpers.inflationAdjust(sel_actual_sum, year, benchmark)
+            sel_est_sum = BudgetHelpers.inflationAdjust(sel_est_sum, year, benchmark)
+            prev_actual_sum = BudgetHelpers.inflationAdjust(prev_actual_sum, year-1, benchmark)
+            prev_est_sum = BudgetHelpers.inflationAdjust(prev_est_sum, year-1, benchmark)
+        }
+        var actualChange = BudgetHelpers.calc_change(sel_actual_sum, prev_actual_sum);
+        var estChange = BudgetHelpers.calc_est_change(sel_est_sum, prev_est_sum, prev_actual_sum);
         this.set({
-            'selectedActual': BudgetHelpers.convertToMoney(actual[index]),
-            'selectedEst': BudgetHelpers.convertToMoney(est[index]),
+            'selectedActual': BudgetHelpers.convertToMoney(sel_actual_sum),
+            'selectedEst': BudgetHelpers.convertToMoney(sel_est_sum),
             'actualChange': actualChange,
             'estChange': estChange,
             'viewYear': year,
