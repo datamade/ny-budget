5a27ffe028487239802a1133146900bd4856cf53
Cathy Deng <cathyd125@gmail.com>
2016-01-13 10:31:03 -0600

    updating data

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 var app = {};
 
 startYear   = 1995;  // first year of budget data
-endYear     = 2016;  // last year of budget data
+endYear     = 2017;  // last year of budget data
 activeYear  = 2016;  // default year to select
 debugMode   = false; // change to true for debugging message in the javascript console
 municipalityName = 'State of New York'; // name of budget municipality 
@@ -12,17 +12,17 @@ actualTitle = 'Actuals';    // title for second series
 
 mergeSeries = true;         // true if estimates & actuals span different years & can be merged into one line, otherwise false
 projectionStartYear = 2016; // if some numbers are projections into the future, set as year of first projection. otherwise, set null. it is assumed that estEndYear = endYear. this determines the chart x axis band
-plotBandBuffer = -7;        // move the plot band start N months back, for styling
+plotBandBuffer = -1;        // move the plot band start N months back, for styling
 
 
 // Inflation Index (for inflation adjusted dollars)
-benchmark = 2015;
+benchmark = 2016;
 inflation_idx = {
     1995: 149.3, 1996: 153.4, 1997: 158.0, 1998: 161.1, 1999: 163.7,
     2000: 167.9, 2001: 173.6, 2002: 177.6, 2003: 181.2, 2004: 184.8,
     2005: 190.3, 2006: 197.0, 2007: 202.8, 2008: 209.5, 2009: 215.2,
     2010: 215.8, 2011: 219.2, 2012: 226.5, 2013: 230.6, 2014: 233.8,
-    2015: 236.7
+    2015: 236.7, 2016: 238.2
 }
 enable_inflation_toggle = true; // by default, show inflation adjusted (real) numbers. toggle to show nominal (unadjusted) numbers
 
