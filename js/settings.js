667b30e69ae3cf3730a1ee46a193a1111eed127e
Derek Eder <derek.eder@gmail.com>
2017-01-17 16:34:29 -0600

    updated budget info, cpi and year settings

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,8 @@
 var app = {};
 
 startYear   = 1995;  // first year of budget data
-endYear     = 2017;  // last year of budget data
-activeYear  = 2017;  // default year to select
+endYear     = 2018;  // last year of budget data
+activeYear  = 2018;  // default year to select
 debugMode   = false; // change to true for debugging message in the javascript console
 municipalityName = 'State of New York'; // name of budget municipality 
 
@@ -11,18 +11,18 @@ estTitle  = 'Estimates';    // title for first series
 actualTitle = 'Actuals';    // title for second series
 
 mergeSeries = true;         // true if estimates & actuals span different years & can be merged into one line, otherwise false
-projectionStartYear = 2017; // if some numbers are projections into the future, set as year of first projection. otherwise, set null. it is assumed that estEndYear = endYear. this determines the chart x axis band
+projectionStartYear = 2018; // if some numbers are projections into the future, set as year of first projection. otherwise, set null. it is assumed that estEndYear = endYear. this determines the chart x axis band
 plotBandBuffer = -7;        // move the plot band start N months back, for styling
 
 
 // Inflation Index (for inflation adjusted dollars)
-benchmark = 2016;
+benchmark = 2017;
 inflation_idx = {
     1995: 149.3, 1996: 153.4, 1997: 158.0, 1998: 161.1, 1999: 163.7,
     2000: 167.9, 2001: 173.6, 2002: 177.6, 2003: 181.2, 2004: 184.8,
     2005: 190.3, 2006: 197.0, 2007: 202.8, 2008: 209.5, 2009: 215.2,
     2010: 215.8, 2011: 219.2, 2012: 226.5, 2013: 230.6, 2014: 233.8,
-    2015: 236.7, 2016: 237.6
+    2015: 236.7, 2016: 237.6, 2017: 241.5, 2018: 247.7
 }
 enable_inflation_toggle = true; // by default, show inflation adjusted (real) numbers. toggle to show nominal (unadjusted) numbers
 
