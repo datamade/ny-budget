074c26abb986c16f24e4a5a91b89db1481a746b1
Derek Eder <derek.eder@gmail.com>
2023-02-01 16:24:09 -0600

    updated data for 2024 executive budget

--- js/settings.js
+++ js/settings.js
@@ -1,8 +1,8 @@
 var app = {};
 
 startYear   = 1995;  // first year of budget data
-endYear     = 2023;  // last year of budget data
-activeYear  = 2023;  // default year to select
+endYear     = 2024;  // last year of budget data
+activeYear  = 2024;  // default year to select
 debugMode   = false; // change to true for debugging message in the javascript console
 municipalityName = 'State of New York'; // name of budget municipality 
 
@@ -11,7 +11,7 @@ estTitle  = 'Estimates';    // title for first series
 actualTitle = 'Actuals';    // title for second series
 
 mergeSeries = true;         // true if estimates & actuals span different years & can be merged into one line, otherwise false
-projectionStartYear = 2022; // if some numbers are projections into the future, set as year of first projection. otherwise, set null. it is assumed that estEndYear = endYear. this determines the chart x axis band
+projectionStartYear = 2023; // if some numbers are projections into the future, set as year of first projection. otherwise, set null. it is assumed that estEndYear = endYear. this determines the chart x axis band
 plotBandBuffer = 0;        // move the plot band start N months back, for styling
 
 
@@ -23,7 +23,7 @@ inflation_idx = {
     2005: 190.3, 2006: 197.0, 2007: 202.8, 2008: 209.5, 2009: 215.2,
     2010: 215.8, 2011: 219.2, 2012: 226.5, 2013: 230.6, 2014: 233.8,
     2015: 236.7, 2016: 237.6, 2017: 241.5, 2018: 246.5, 2019: 252.1, 
-    2020: 257.0, 2021: 260.1, 2022: 276.2, 2023: 298.0 
+    2020: 257.0, 2021: 260.1, 2022: 276.2, 2023: 296.7 
 }
  
 enable_inflation_toggle = true; // by default, show inflation adjusted (real) numbers. toggle to show nominal (unadjusted) numbers
